eee7957854552df85f4a0c3a470e032eeb37be1c
François Rousselet <francois.rousselet@icloud.com>
2021-05-14 00:13:53 +0200

    Dynamic ZSH plugins loading

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -2,7 +2,7 @@ export DOTCONFPATH="$HOME/.dotfiles/zsh"
 
 source "$DOTCONFPATH/setup.zsh"
 source "$DOTCONFPATH/prompt.zsh"
-source "$DOTCONFPATH/plugins.zsh"
 source "$DOTCONFPATH/ruby.zsh"
 source "$DOTCONFPATH/pyenv.zsh"
+source "$DOTCONFPATH/plugins.zsh"
 source "$DOTCONFPATH/extra.zsh"
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,2 +1,49 @@
-plugins=(git pyenv aws docker osx ansible terraform node man brew genpass zsh-autosuggestions zsh-syntax-highlighting)
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
+plugins=()
+
+if command -v git > /dev/null; then
+    plugins+=git
+fi
+
+if command -v pyenv > /dev/null; then
+    plugins+=pyenv
+fi
+
+if command -v docker > /dev/null; then
+    plugins+=docker
+fi
+
+if command -v ansible > /dev/null; then
+    plugins+=ansible
+fi
+
+if command -v terraform > /dev/null; then
+    plugins+=terraform
+fi
+
+if command -v node > /dev/null; then
+    plugins+=node
+fi
+
+if command -v man > /dev/null; then
+    plugins+=man
+fi
+
+if command -v brew > /dev/null; then
+    plugins+=brew
+fi
+
+if [[ -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]]
+then
+    plugins+=zsh-autosuggestions
+fi
+
+if [[ -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]]
+then
+    plugins+=zsh-syntax-highlighting
+fi
+
+plugins+=genpass
+
 SHOW_AWS_PROMPT=false
