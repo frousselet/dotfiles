a05f2687d3c6136a664c4964f5f7b2bcb2639311
François Rousselet <francois.rousselet@icloud.com>
2021-06-04 23:03:15 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -2,29 +2,34 @@
 
 GITHUB="https://github.com/frousselet/dotfiles.git"
 
-if [[ ! command -v brew > /dev/null ]]
-    printf "Installing Homebrew...\n"
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# Clone dotfiles
-if [[ ! command -v git > /dev/null ]]
-then
-    printf "Installing Git...\n"
-    if [[ "$OSTYPE" == "darwin"* ]]
-    then
-        brew install git
-    elif [[ lsb_release -d | grep "ubuntu"]] || [[ lsb_release -d | grep "ubuntu" ]]
-    then
-        sudo apt-get -y install git
+# OSX: Check Homebrew installation
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ ! command -v brew > /dev/null ]]; then
+        printf "Installing Homebrew...\n"
+        bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
-    exit 1
 fi
 
+# ALL: Check Git installation
+if [[ ! command -v git > /dev/null ]]; then
+    printf "Installing Git...\n"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install git
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        if [[ lsb_release -d | grep "ubuntu"]] || [[ lsb_release -d | grep "ubuntu" ]]; then
+            sudo apt-get -y install git
+        else
+            printf "git not installed!\n"
+            exit 1
+        fi
+    fi
+fi
+
+# ALL: Clone dotfiles
 rm -rf "$HOME/.dotfiles"
 git clone "$GITHUB" "$HOME/.dotfiles"
 
-# Install Oh-My-ZSH
+# ALL: Install Oh-My-ZSH
 if command -v curl > /dev/null; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 elif command -v wget > /dev/null; then
@@ -34,7 +39,6 @@ else
     exit 1
 fi
 
-# Enable ZSH configuration
+# ALL: Enable ZSH configuration
 ln -sf "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.zshrc"
-
 . "$HOME/.zshrc"
