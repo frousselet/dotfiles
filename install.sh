efda9efdedd860965c70d7b1038e894cb4cb4a3d
François Rousselet <francois.rousselet@icloud.com>
2021-06-04 22:56:37 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
+GITHUB="https://github.com/frousselet/dotfiles.git"
+
+if [[ ! command -v brew > /dev/null ]]
+    printf "Installing Homebrew...\n"
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 # Clone dotfiles
-if command -v git > /dev/null; then
-    rm -rf $HOME/.dotfiles
-    git clone https://github.com/frousselet/dotfiles.git $HOME/.dotfiles
-else
-    printf "Git not installed!"
+if [[ ! command -v git > /dev/null ]]
+then
+    printf "Installing Git...\n"
+    if [[ "$OSTYPE" == "darwin"* ]]
+    then
+        brew install git
+    elif [[ lsb_release -d | grep "ubuntu"]] || [[ lsb_release -d | grep "ubuntu" ]]
+    then
+        sudo apt-get -y install git
+    fi
     exit 1
 fi
+
+rm -rf "$HOME/.dotfiles"
+git clone "$GITHUB" "$HOME/.dotfiles"
 
 # Install Oh-My-ZSH
 if command -v curl > /dev/null; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
+elif command -v wget > /dev/null; then
     sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+else
+    printf "curl or wget not installed!\n"
+    exit 1
 fi
 
 # Enable ZSH configuration
 ln -sf "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.zshrc"
 
-. $HOME/.zshrc
+. "$HOME/.zshrc"
