8e5ee4ff2d56fb2507da8aa5c095c7a7fb7d4ddf
François Rousselet <francois.rousselet@idemia.com>
2021-06-15 14:02:29 +0200

    Disable prompt version when command does not exist

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,85 +1,101 @@
 docker_version() {
-	if [ -f "Dockerfile" ]
-	then
-		echo " [docker:$(docker --version 2> /dev/null | cut -d ',' -f 1 | sed 's/Docker version //g')]"
+	if command -v docker > /dev/null; then
+		if [ -f "Dockerfile" ]
+		then
+			echo " [docker:$(docker --version 2> /dev/null | cut -d ',' -f 1 | sed 's/Docker version //g')]"
+		fi
 	fi
 }
 
 node_version() {
-	if [ -f "package.json" ]
-	then
-		echo " [node:$(node -v | sed 's/v//g')] [$(jq -r '.name' < package.json):$(jq -r '.version' < package.json)]"
+	if command -v node > /dev/null; then
+		if [ -f "package.json" ]
+		then
+			echo " [node:$(node -v | sed 's/v//g')] [$(jq -r '.name' < package.json):$(jq -r '.version' < package.json)]"
+		fi
 	fi
 }
 
 python_version() {
-	py_file_count="$(find . -maxdepth 1 -type f -name '*.py')"
-	if [ $py_file_count ]
-	then
-		echo " [python:$(python --version | sed 's/Python //g')]"
+	if command -v python > /dev/null; then
+		py_file_count="$(find . -maxdepth 1 -type f -name '*.py')"
+		if [ $py_file_count ]
+		then
+			echo " [python:$(python --version | sed 's/Python //g')]"
+		fi
 	fi
 }
 
 terraform_version() {
-	tf_file_count="$(find .  -maxdepth 1 -type f -name '*.tf')"
-	if [ $tf_file_count ]
-	then
-		echo " [terraform:$(tfenv version-name 2> /dev/null)]"
+	if command -v terraform > /dev/null; then
+		tf_file_count="$(find .  -maxdepth 1 -type f -name '*.tf')"
+		if [ $tf_file_count ]
+		then
+			echo " [terraform:$(tfenv version-name 2> /dev/null)]"
+		fi
 	fi
 }
 
 vagrant_version() {
-	if [ -f "Vagrantfile" ]
-	then
-		echo " [vagrant:$(vagrant --version 2> /dev/null | cut -d ' ' -f 2)]"
+	if command -v vagrant > /dev/null; then
+		if [ -f "Vagrantfile" ]
+		then
+			echo " [vagrant:$(vagrant --version 2> /dev/null | cut -d ' ' -f 2)]"
+		fi
 	fi
 }
 
 git_branch() {
-	gb="$(git branch 2> /dev/null | grep '*' | sed 's/* //')"
-	if [ $gb ]
-	then
-		gs_M="$(git status --short 2> /dev/null | grep "^ M" | wc -l | sed 's/ //g' | sed 's/\n//g' | sed 's/0//g')"
-		gs_D="$(git status --short 2> /dev/null | grep "^ D" | wc -l | sed 's/ //g' | sed 's/\n//g' | sed 's/0//g')"
-		gs_U="$(git status --short 2> /dev/null | grep "^??" | wc -l | sed 's/ //g' | sed 's/\n//g' | sed 's/0//g')"
-		gs_A="$(git status --short 2> /dev/null | grep "^[A|M]" | wc -l | sed 's/ //g' | sed 's/\n//g' | sed 's/0//g')"
-		gs_P="$(git log origin/$gb..$gb 2> /dev/null | wc -l | sed 's/ //g' | sed 's/\n//g' | sed 's/0//g')"
-		if [ $gs_U ]
+	if command -v git > /dev/null; then
+		gb="$(git branch 2> /dev/null | grep '*' | sed 's/* //')"
+		if [ $gb ]
 		then
-			gb="$gb/+"
+			gs_M="$(git status --short 2> /dev/null | grep "^ M" | wc -l | sed 's/ //g' | sed 's/\n//g' | sed 's/0//g')"
+			gs_D="$(git status --short 2> /dev/null | grep "^ D" | wc -l | sed 's/ //g' | sed 's/\n//g' | sed 's/0//g')"
+			gs_U="$(git status --short 2> /dev/null | grep "^??" | wc -l | sed 's/ //g' | sed 's/\n//g' | sed 's/0//g')"
+			gs_A="$(git status --short 2> /dev/null | grep "^[A|M]" | wc -l | sed 's/ //g' | sed 's/\n//g' | sed 's/0//g')"
+			gs_P="$(git log origin/$gb..$gb 2> /dev/null | wc -l | sed 's/ //g' | sed 's/\n//g' | sed 's/0//g')"
+			if [ $gs_U ]
+			then
+				gb="$gb/+"
+			fi
+			if [ $gs_M ]
+					then
+							gb="$gb/~"
+					fi
+			if [ $gs_D ]
+					then
+							gb="$gb/-"
+					fi
+			if [ $gs_A ]
+					then
+							gb="$gb/⋯"
+					fi
+			if [ $gs_P ]
+					then
+							gb="$gb/↑"
+					fi
+			echo " [git:$gb]"
 		fi
-		if [ $gs_M ]
-                then
-                        gb="$gb/~"
-                fi
-		if [ $gs_D ]
-                then
-                        gb="$gb/-"
-                fi
-		if [ $gs_A ]
-                then
-                        gb="$gb/⋯"
-                fi
-		if [ $gs_P ]
-                then
-                        gb="$gb/↑"
-                fi
-		echo " [git:$gb]"
 	fi
 }
 
 aws_profile() {
-	if [ $AWS_PROFILE ]
-	then
-		echo " [aws:$AWS_PROFILE]"
+	if command -v aws > /dev/null; then
+		if [ $AWS_PROFILE ]
+		then
+			echo " [aws:$AWS_PROFILE]"
+		fi
 	fi
 }
 
 gcp_profile() {
-	gcp=$(gcloud config get-value core/project)
-	if [ $gcp ]
-	then
-		echo " [gcp:$gcp]"
+	if command -v gcloud > /dev/null; then
+		gcp=$(gcloud config get-value core/project)
+		if [ $gcp ]
+		then
+			echo " [gcp:$gcp]"
+		fi
 	fi
 }
 
