ff5d5b384b54b322c03978c4112f684e619501d9
François Rousselet <francois.rousselet@icloud.com>
2021-12-21 21:53:46 +0100

    [ZSH] Add ZSH completion for Homebrew

--- zsh/gcloud.zsh
+++ zsh/gcloud.zsh
@@ -1,6 +1,5 @@
-
-# The next line updates PATH for the Google Cloud SDK.
 if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/google-cloud-sdk/path.zsh.inc"; fi
-
-# The next line enables shell command completion for gcloud.
 if [ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/google-cloud-sdk/completion.zsh.inc"; fi
+
+# In case gcloud in installed via brew (brew install google-cloud-sdk)
+if [ -f "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc" ]; then source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"; fi
