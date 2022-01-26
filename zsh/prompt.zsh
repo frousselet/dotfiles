7c7f04fb352490dc1dbe30cfdfcd46e130e6ede5
François Rousselet <francois.rousselet@rslt.fr>
2022-01-26 22:01:54 +0100

    [ZSH] Update prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -103,4 +103,4 @@ preexec() {
 	printf "\n"
 }
 
-PS1=$'\n\n%{$reset_color%}%(!.%{$fg[red]%}.%{$reset_color%})%B%n%b%{$reset_color%} %(!.%{$fg[red]%}.%{$fg[cyan]%})@%{$reset_color%} %B%m :%b %(2~|%(!.%{$fg[red]%}.%{$fg[cyan]%})⋯%{$reset_color%}/%1~|%~)%(!.%{$fg[red]%}.%{$fg[cyan]%})$(git_branch)$(aws_profile)$(terraform_version)%{$reset_color%}\n%(?.%{$reset_color%}.%{$fg[red]%})%B->%b%{$reset_color%} '
+PS1=$'\n\n%{$reset_color%}%n%{$reset_color%} @ %m : %(2~|⋯%{$reset_color%}/%1~|%~)%(!.%{$fg[red]%}.%{$fg[cyan]%})$(git_branch)$(aws_profile)$(terraform_version)%{$reset_color%}\n%(?.%{$reset_color%}.%{$fg[red]%})%(!.%{$fg[red]%}.%{$reset_color%})%B->%b%{$reset_color%} '
