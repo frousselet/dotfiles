299c06e976bdcfa0ea322ce590290109ca2c0143
François Rousselet <francois.rousselet@rslt.fr>
2022-04-06 15:48:47 +0200

    Add install_macos.sh

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Dotfiles
 
 ![MacOS Terminal](doc/macos_terminal.png "MacOS Terminal")
+
+Install on MacOS:
+
+```bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/frousselet/dotfiles/HEAD/install_macos.sh)"
+```
---
+++ install_macos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if ! command -v brew > /dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if command -v brew > /dev/null; then
+  brew update
+  brew install zsh git wget
+  brew install --cask drawio rectangle tower netnewswire
+  brew install --build-from-source terraform
+fi
+
+git config --global user.email "francois.rousselet@rslt.fr"
+git config --global user.name "François Rousselet"
+
+cd /home/$USER
+
+git clone https://github.com/frousselet/dotfiles.git .dotfiles
+rm -rf .zshrc
+ln -s .dotfiles/.zshrc .zshrc
+
+ln -s /Users/$USER/Library/Mobile\ Documents/com\~apple\~CloudDocs/Projets Projets
